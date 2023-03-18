9e3d1b9cab7774346a1f25de5525595656ad6148
RHoganD <richpatric@hotmail.com>
2023-03-18 22:55:13 +0000

    add the funcion to start quiz and the menu

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ from Question import Question
 from Question import easy_question_answer
 from Question import medium_questions_answer
 from Question import hard_questions_answer
-
 import pyfiglet
 from pyfiglet import figlet_format
 import random
-from string import ascii_lowercase
+
+# from string import ascii_lowercase
 
 
 SCOPE = [
@@ -41,6 +41,7 @@ def run_quiz(questions):
             correct += 1
             print("⭐ Correct! ⭐ You got 10 point\n")
         else:
+            incorrect += 1
             score = -10
             print("Wrong answer!! You lost 10 points\n")
 
@@ -53,11 +54,76 @@ def run_quiz(questions):
         if question == hard_questions_answer:
             question = "Hard"
             score += 30
-    print("Great!!", name,  "Your got " + int(score) + " Correct / out of " + str(len(questions)),  "questions\n")
-    print("Total score: " + str(score) + "  answering" + (correct) + (incorrect))
+    print(f'(Great!! {name} Your got  {correct} Correct / out of   {len(questions)}  questions)')
+    print(f'(Total score:  {score}  answering {correct} and {incorrect} Incorrect)')
         
 
-run_quiz(easy_question_answer)
+start()
+
+
+def start():
+    """
+    Shows the start of the quiz and greetings
+    """
+    slogan = pyfiglet.figlet_format("The Quiz", font = "slant" )
+    print(slogan)
+
+    name = input("Enter your name: \n")
+    
+    print("\n")
+    print("Hello", name, "Welcome to the quiz\n")
+    playing = input("Do you want to play the quiz? ")
+
+    if playing != "yes":
+        print("Sorry you are leaving, lets play another time")
+        quit()
+    else:
+        print("Okay! Let's Play! :) \n")
+        run_quiz()
+  
+menu()
+
+def menu(questions):
+    """
+    Shows the option to choose question dificulties
+    """
+    print(  "Please select Quiz Level\n\n   ")
+    print("1. Easy")
+    Print("2. Medium")
+    Print("3. Hard")
+    Print("4. Leaderboard")
+    Print("0. Quit")
+    levels = input("Enter 1, 2, 3, 4 or 0: ")
+
+    if levels == "1.":
+        print("Loanding Easy question......")
+        clear()
+        start_quiz(easy_question_answer)
+
+    if levels == "2":
+        print("Loanding Medium question......")
+        clear()
+        start_quiz(medium_questions_answer)
+
+    if levels == "3":
+        print("Loanding Hard question......")
+        clear()
+        start_quiz(hard_questions_answer)
+
+    if levels == "4":
+        show_leaderboard()
+
+    if levels == "0":
+        print("Thanks for playing!")
+        quit()
+
+start_quiz()
+
+
+
+
+
+
 
 
 
